d944e301534dd0bbe1ebd444825f250f4316e3bb
Mariana <mariana@fastmail.net>
2021-04-03 12:55:50 +0200

    create new file - CategoryTableViewController

---
+++ ToDoList/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryTableViewController.swift
+//  ToDoList
+//
+//  Created by Mariana Steblii on 03/04/2021.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
